788b57a8b72e82fc3e2d3ef23ff855c51a87a84c
IvanVerboloz <87490731+IvanVerboloz@users.noreply.github.com>
2025-07-14 23:39:32 +0300

    correction

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const userNum = prompt("Введіть трицифрове число (наприклад: 321):");
 const validNum = userNum?.trim() || "";
 
-if (validNum.length !== 3 || !Number.isInteger(+validNum)) {
+const number = Number(validNum);
+const isInt = Number.isInteger(number);
+const absStr = Math.abs(number).toString();
+
+if (!isInt || absStr.length !== 3) {
   alert("Помилка: введіть саме ТРИЦИФРОВЕ ціле число!");
 } else {
-  const [a, b, c] = validNum;
+  const [a, b, c] = absStr;
 
   if (a === b && b === c) {
     alert("Усі цифри однакові");
